1628d48be546e151dc0f3f65ed1bbf021a54be22
Corbin Crutchley <crutchcorn@gmail.com>
2021-01-26 15:21:56 -0800

    Add initial integration of Shoelace

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
+import "@shoelace-style/shoelace/dist/shoelace/shoelace.css";
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { useLayoutEffect, useRef } from "react";
+
+import {
+  setAssetPath,
+  defineCustomElements
+} from "@shoelace-style/shoelace";
+
+function CustomEls({ URL }) {
+  const customEls = useRef(false);
+
+  useLayoutEffect(() => {
+    if (customEls.current) {
+      return;
+    }
+    setAssetPath(`${URL}/static/static`);
+    // If you're wanting to selectively import components, replace this line with your own definitions
+    defineCustomElements();
+    // customElements.define("sl-button", SlButton);
+    customEls.current = true;
+  }, [URL, customEls]);
+
+  return null;
 }
+
+function MyApp({ Component, pageProps, URL }) {
+  return (
+    <>
+      {process.browser && <CustomEls URL={URL} />}
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+MyApp.getInitialProps = async (context) => {
+  const URL = process.env.BASE_URL;
+
+  return {
+    URL,
+  };
+};
 
 export default MyApp
